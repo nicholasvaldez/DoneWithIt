39777ec648904b60b077b2870bf8b8c83c715ccc
Nick Valdez <nicholastvaldez@gmail.com>
2023-09-24 15:26:29 -0500

    quick align-items fix

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
+import { View } from "react-native"
+import AppButton from "./app/components/AppButton"
 import ViewImageScreen from "./app/screens/ViewImageScreen"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 
 export default function App() {
-  return <ViewImageScreen />
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "flex-start",
+      }}
+    >
+      <AppButton title="Login" onPress={() => console.log("click")} />
+    </View>
+  )
 }
